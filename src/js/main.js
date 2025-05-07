56411abc6d35c7dfef2d9b06a2b6a808fe9d05f8
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2025-05-07 08:52:53 -0500

    feat: :sparkles: Creacion para Guardar en Local Storage

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,13 @@
 document.getElementById('formulario').addEventListener('submit', function(e) {
-    e.preventDefault();
+
+    const evento = {
+      nombre: document.getElementById('nombreEvento').value,
+      lugar: document.getElementById('lugar').value,
+      fechaInicio: document.getElementById('fechaInicio').value,
+      fechaFin: document.getElementById('fechaFin').value,
+      horario: document.getElementById('horario').value
+    };
+
+
+    localStorage.setItem('eventoGuardado', JSON.stringify(evento));
+  });
